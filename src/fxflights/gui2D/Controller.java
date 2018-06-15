04c59b1e69295946560e971f52cc70100e2e16be
Konrad Powęska <konradpoweska5@gmail.com>
2018-06-15 14:03:08 +0200

    UI 2D base

--- src/fxflights/gui2D/Controller.java
+++ src/fxflights/gui2D/Controller.java
@@ -1,14 +1,23 @@
 package fxflights.gui2D;
 
 import javafx.fxml.FXML;
+import javafx.scene.SubScene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 
 public class Controller {
+
+	@FXML SubScene earth3D;
+	@FXML ChoiceBox fromChoiceBox;
+	@FXML ChoiceBox toChoiceBox;
+	@FXML ListView flightsList;
+	@FXML Button searchButton;
 
     public Controller() {}
 
     @FXML
     public void initialize() {
-        
 
        
 
