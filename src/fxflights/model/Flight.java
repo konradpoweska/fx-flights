b032185282325accb20538ff1eab20e475a20dcf
Jules Paget <jules25115@hotmail.fr>
2018-06-21 21:17:33 +0200

    save

--- src/fxflights/model/Flight.java
+++ src/fxflights/model/Flight.java
@@ -123,7 +123,7 @@ public class Flight {
 	 */
 	@Override
 	public String toString() {
-		return icaoFlight + ", " + company + " from: " + from + " to: " + to + ". Currently at lat: " + latitude + ", long:" + longitude + ".";
+		return icaoFlight + ", " + company + " from: " + from + " to: " + to + ". Currently at lat: " + latitude + ", long:" + longitude + " at angle : " + angle + " .";
 	}
 	
 	
